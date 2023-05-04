4922127d45882c2fd0833d55f72d5cfb505a66f7
Hannah Del Real <hdelreal@my365.bellevue.edu>
2023-05-03 23:23:18 -0700

    fixed syntax error

--- models/appointment.js
+++ models/appointment.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 let appointmentSchema = new Schema ({
-    userName: { type: String, required: true },
+    userName: { type: String, required: true, unique: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     service: { type: String, required: true }
 });
 
